8597b88637c0ce4046d412f3020b021878f89d29
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2024-06-06 19:11:58 +0200

    🍇

--- src/main/kotlin/no/nav/eux/slett/usendte/rinasaker/kafka/EuxRinaCaseEventsKafkaListener.kt
+++ src/main/kotlin/no/nav/eux/slett/usendte/rinasaker/kafka/EuxRinaCaseEventsKafkaListener.kt
@@ -1,5 +1,6 @@
 package no.nav.eux.slett.usendte.rinasaker.kafka
 
+import com.fasterxml.jackson.annotation.JsonProperty
 import io.github.oshai.kotlinlogging.KotlinLogging.logger
 import org.apache.kafka.clients.consumer.ConsumerRecord
 import org.springframework.kafka.annotation.KafkaListener
@@ -15,23 +16,43 @@ class EuxRinaCaseEventsKafkaListener {
         log.info { "Received case kafka message: $value" }
     }
 
-    @KafkaListener(id = "eux-slett-usendte-rinasaker-document-draft-3", topics = ["eessibasis.eux-rina-document-events-v1"])
+    @KafkaListener(
+        id = "eux-slett-usendte-rinasaker-document-draft-3",
+        topics = ["eessibasis.eux-rina-document-events-v1"]
+    )
     fun document(value: String?) {
         log.info { "Received document kafka message: $value" }
     }
 
-    @KafkaListener(id = "eux-slett-usendte-rinasaker-document-draft-4", topics = ["eessibasis.eux-rina-document-events-v1"])
+    @KafkaListener(
+        id = "eux-slett-usendte-rinasaker-document-draft-4",
+        topics = ["eessibasis.eux-rina-document-events-v1"]
+    )
     fun document(consumerRecord: ConsumerRecord<String, Doc>) {
         log.info { "Received document kafka message (data class): $consumerRecord, doc=${consumerRecord.value()}" }
+        log.info { "Received document kafka message, caseId: ${consumerRecord.value().payload.documentMetadata.caseId}" }
     }
 
-    @KafkaListener(id = "eux-slett-usendte-rinasaker-notification-draft-3", topics = ["eessibasis.eux-rina-notification-events-v1"])
+    @KafkaListener(
+        id = "eux-slett-usendte-rinasaker-notification-draft-3",
+        topics = ["eessibasis.eux-rina-notification-events-v1"]
+    )
     fun notification(value: String?) {
         log.info { "Received notification kafka message: $value" }
     }
 
     data class Doc(
         val documentEventType: String,
-        val buc: String
+        val buc: String,
+        val payload: Payload
+    )
+
+    data class Payload(
+        @JsonProperty("DOCUMENT_METADATA")
+        val documentMetadata: DocumentMetadata
+    )
+
+    data class DocumentMetadata(
+        val caseId: String
     )
 }
