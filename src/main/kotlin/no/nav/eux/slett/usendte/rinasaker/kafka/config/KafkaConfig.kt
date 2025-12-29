27f60b03bffb946fc44f0fc4b4702f97595c86ea
Vegard Hillestad <vegard.lundeberg.hillestad@nav.no>
2025-12-29 11:22:33 +0100

    boot4

--- src/main/kotlin/no/nav/eux/slett/usendte/rinasaker/kafka/config/KafkaConfig.kt
+++ src/main/kotlin/no/nav/eux/slett/usendte/rinasaker/kafka/config/KafkaConfig.kt
@@ -9,20 +9,21 @@ import org.apache.kafka.common.serialization.StringDeserializer
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.kafka.annotation.EnableKafka
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory
 import org.springframework.kafka.core.ConsumerFactory
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer.VALUE_DESERIALIZER_CLASS
-import org.springframework.kafka.support.serializer.JsonDeserializer
-import org.springframework.kafka.support.serializer.JsonDeserializer.VALUE_DEFAULT_TYPE
+import org.springframework.kafka.support.serializer.JacksonJsonDeserializer
 
 @Configuration
+@EnableKafka
 class KafkaConfig(
-    @Value("\${kafka.bootstrap-servers}")
+    @param:Value("\${kafka.bootstrap-servers}")
     val bootstrapServers: String,
-    @Value("\${kafka.properties.security.protocol}")
+    @param:Value("\${kafka.properties.security.protocol}")
     val securityProtocol: String,
     val kafkaSslProperties: KafkaSslProperties
 ) {
@@ -33,19 +34,22 @@ class KafkaConfig(
     @Bean
     fun rinaDocumentKafkaListenerContainerFactory() = kafkaListenerContainerFactory<KafkaRinaDocument>()
 
-    private inline fun <reified T> kafkaListenerContainerFactory() =
+    private inline fun <reified T : Any> kafkaListenerContainerFactory() =
         ConcurrentKafkaListenerContainerFactory<String, T>()
-            .apply { consumerFactory = docConsumerFactory<T>() }
+            .apply { setConsumerFactory(docConsumerFactory<T>()) }
 
-    private inline fun <reified T> docConsumerFactory(): ConsumerFactory<String, T> =
-        DefaultKafkaConsumerFactory(
+    private inline fun <reified T : Any> docConsumerFactory(): ConsumerFactory<String, T> {
+        val jsonDeserializer = JacksonJsonDeserializer(T::class.java).apply {
+            addTrustedPackages("*")
+        }
+
+        return DefaultKafkaConsumerFactory(
             mapOf(
                 BOOTSTRAP_SERVERS_CONFIG to bootstrapServers,
                 KEY_DESERIALIZER_CLASS_CONFIG to StringDeserializer::class.java,
                 VALUE_DESERIALIZER_CLASS_CONFIG to ErrorHandlingDeserializer::class.java,
                 KEY_DESERIALIZER_CLASS to StringDeserializer::class.java.name,
-                VALUE_DESERIALIZER_CLASS to JsonDeserializer::class.java.name,
-                VALUE_DEFAULT_TYPE to T::class.java.name,
+                VALUE_DESERIALIZER_CLASS to JacksonJsonDeserializer::class.java.name,
                 SECURITY_PROTOCOL_CONFIG to securityProtocol,
                 SSL_KEYSTORE_TYPE_CONFIG to kafkaSslProperties.keystore.type,
                 SSL_KEYSTORE_LOCATION_CONFIG to kafkaSslProperties.keystore.location,
@@ -53,6 +57,9 @@ class KafkaConfig(
                 SSL_TRUSTSTORE_TYPE_CONFIG to kafkaSslProperties.truststore.type,
                 SSL_TRUSTSTORE_LOCATION_CONFIG to kafkaSslProperties.truststore.location,
                 SSL_TRUSTSTORE_PASSWORD_CONFIG to kafkaSslProperties.truststore.password
-            )
+            ),
+            StringDeserializer(),
+            ErrorHandlingDeserializer(jsonDeserializer)
         )
+    }
 }
